fdaa66747678bf91d95347d24149e82abcec1748
Pedro Avila <pedroulisesavila@gmail.com>
2024-08-22 16:43:42 -0500

    Implememtacion del componente subTitle

--- src/screens/home/HomePage.tsx
+++ src/screens/home/HomePage.tsx
@@ -1,10 +1,12 @@
 import { HeaderComponent } from "../../shared/components/header-component";
+import { SubTitleComponent } from "../sub-title/SubTitle";
 
 export const HomePage = () => {
   return (
     <div className="flex flex-col h-full justify-between">
-      <header>
+      <header className="space-y-8">
         <HeaderComponent />
+        <SubTitleComponent />
       </header>
       <main>main</main>
       <footer>footer</footer>
